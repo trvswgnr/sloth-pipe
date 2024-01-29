f2537317e9ed9ed6823014b0485cf51d70e26bd5
Travis Wagner <travis.wagner@umusic.com>
2024-01-29 14:24:59 -0500

    refactor Pipe function to optimize execution

--- pipe.ts
+++ pipe.ts
@@ -3,9 +3,38 @@ import Queue from "./fifo-queue";
 const NODE_INSPECT = Symbol.for("nodejs.util.inspect.custom");
 
 export const Pipe = <const T>(value: T): Pipeable<T> => {
+    let hasTap = false;
+    let hasCatch = false;
     const queue = new Queue<QueueItem<T>>();
     const exec = (): unknown => {
         if (queue.empty()) return value;
+        if (!hasTap && !hasCatch) {
+            switch (queue.length) {
+                case 0:
+                    return value;
+                case 1: {
+                    const item = queue.dequeue()!;
+                    value = item.fn(value, ...item.args);
+                    return value;
+                }
+                case 2: {
+                    const item1 = queue.dequeue()!;
+                    const item2 = queue.dequeue()!;
+                    value = item2.fn(item1.fn(value, ...item1.args), ...item2.args);
+                    return value;
+                }
+                case 3: {
+                    const item1 = queue.dequeue()!;
+                    const item2 = queue.dequeue()!;
+                    const item3 = queue.dequeue()!;
+                    // prettier-ignore
+                    value = item3.fn(item2.fn(item1.fn(value, ...item1.args), ...item2.args), ...item3.args);
+                    return value;
+                }
+                default:
+                    break;
+            }
+        }
         for (const { fn, args, tap, catchFn } of queue.drain()) {
             if (tap) {
                 if (catchFn) {
@@ -32,12 +61,14 @@ export const Pipe = <const T>(value: T): Pipeable<T> => {
             return ret;
         },
         tap: (fn: (x?: any, ...args: any[]) => any, ...args: any[]) => {
+            hasTap = true;
             queue.enqueue({ fn, args, tap: true, catchFn: undefined });
             return ret;
         },
         catch: (fn: (err: unknown) => any) => {
+            hasCatch = true;
             const item = queue.peekBack();
-            if (!item) return ret;
+            if (!item) throw new Error("cannot catch without a previous function");
             item.catchFn = fn as any;
             return ret;
         },
